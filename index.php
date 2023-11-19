c2753e6f8d9a0deaed7afa680aafb9e19cbb9159
Samuel Dellicour <samuel.dellicour@gmail.com>
2023-11-19 22:16:38 +0100

    index changes

--- index.php
+++ index.php
@@ -3,16 +3,19 @@
 declare(strict_types=1);
 
 spl_autoload_register();
-
 //spl_autoload_register(function ($class) {
 //    str_replace('\\', DIRECTORY_SEPARATOR, $class);
 //    require basePath("App/{$class}.php");
 //});
 
+use App\Business\ItemService;
+
 require_once __DIR__ . '/App/Helpers/helpers.php';
 
 
-echo 'hello';
+
+
+echo 'bingo';
 
 
 
