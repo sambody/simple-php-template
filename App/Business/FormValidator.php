fa2d7bf5363fa77c8959d868df00cd38285f7fe5
Samuel Dellicour <samuel.dellicour@gmail.com>
2024-03-10 23:00:54 +0100

    changes to FormValidator, imported from eindproef

--- App/Business/FormValidator.php
+++ App/Business/FormValidator.php
@@ -17,15 +17,27 @@ class FormValidator
      * ValidationService::string($yourVar)
      * ValidationService::string($yourVar, 6, 255)
      * */
-    public static function string($value, $minimumChars = 1, $maximumChars = INF): bool
+    public static function string(string $value, int $minimumChars = 1, int|float $maximumChars = INF): bool
     {
         $value = trim($value);
 
         return strlen($value) >= $minimumChars && strlen($value) <= $maximumChars;
     }
 
-    public static function email($email)
+    // return the email if valid, or false
+    public static function email(string $email): string|bool
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    public static function number(int $number, int $minimumValue = 0, int|float $maximumValue = INF): bool
+    {
+        return $number >= $minimumValue && $number <= $maximumValue;
+    }
+
+    public static function postcode(string $postcode): bool
+    {
+        // Todo: check directly if it is one of available plaatsen in DB? Or keep separate validations?
+        return ctype_digit($postcode) && strlen($postcode) === 4;
     }
 }
